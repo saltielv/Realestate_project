00fa36062511e6f4c7bdc509caca260e3246c2d5
Saltiel Villanueva <saltielvillanueva@gmail.com>
2019-08-08 18:17:16 -0500

    Media folder settings, listing model updated & adding data

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -128,3 +128,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'real_estate/static')
 ]
+
+#Media folder settings
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
