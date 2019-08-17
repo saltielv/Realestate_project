35aa191c612ea931bd59cbb0f0fcbd61db3a0211
Saltiel Villanueva <saltielvillanueva@gmail.com>
2019-08-17 15:08:21 -0500

    User registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,43 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
-        # Register user
-        messages.error(request, 'Test alerts')
-        return redirect('register')
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        #Check if passwords match
+        if password != password2:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+        
+        #Check username
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'That user is taken')
+            return redirect('register')
+
+        #Check email
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'That email is being used')
+            return redirect('register')
+
+        #Looks good
+        user = User.objects.create_user(username=username, password=password,email=email, first_name=first_name, last_name=last_name)
+        # Login after register
+        # auth.login(request, user)
+        # messages.success(request, 'You are now logged in')
+        # return redirect('index')
+        user.save()
+        messages.success(request, 'You are now registered and can log in')
+        return redirect('login')
     else:
         return render(request, 'accounts/register.html')
 
