dfa6d32c851d3cd9d02c3b91f9818eb2bf8e79c8
Saltiel Villanueva <saltielvillanueva@gmail.com>
2019-08-26 21:50:27 -0500

    Contacts admin customization

--- contacts/models.py
+++ contacts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from datetime import datetime
 # Create your models here.
 
-class Contacts(models.Model):
+class Contact(models.Model):
     listing = models.CharField(max_length=200)
     listing_id = models.IntegerField()
     name = models.CharField(max_length=200)
